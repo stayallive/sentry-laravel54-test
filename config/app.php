e1a0d207aad6907d541adcb0ae632f487fd21af3
Alex Bouma <me@alexbouma.me>
2018-01-23 23:34:57 +0100

    Add Sentry Service Provider

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+		Sentry\SentryLaravel\SentryLaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
